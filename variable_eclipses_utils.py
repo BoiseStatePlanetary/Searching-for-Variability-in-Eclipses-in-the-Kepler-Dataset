e1ccdfcfa7ca6520b58e2f7b4ede1fe05ab82db1
Brian Jackson <decaelus@gmail.com>
2018-10-07 10:21:41 -0600

    Next step in analysis
    Added emcee analysis
    
    Added several utility functions to variable_eclipses_utils.py

--- variable_eclipses_utils.py
+++ variable_eclipses_utils.py
@@ -4,8 +4,72 @@
 import numpy as np
 from glob import glob
 from lightkurve import KeplerLightCurveFile
-
+from astropy import units as u
 from transit_utils import median_boxcar_filter, flag_outliers, transit_duration, fit_eclipse_bottom
+from evilmc import evparams, evmodel, convert_Kz
+
+class Kepler76_params:
+    KIC = 4570949
+
+    # The system parameters as reported in Faigler et al. (2013) --
+    #  http://iopscience.iop.org/article/10.1088/0004-637X/771/1/26/meta
+    a = 1./0.221 #\pm 0.003
+    b = 0.944 # \pm 0.011
+    inc = 78.0 # \pm 0.2
+    per = 1.54492875*u.day # \pm 0.00000027
+    Kz_m_per_s = 308. # \pm 20.
+    Kz = convert_Kz(Kz=Kz_m_per_s) # convert to fraction of the speed of light
+
+    Mp = 2.00*u.jupiterMass # \pm 0.26
+    Rp_over_a = 0.0214 # \pm 0.0008
+    Rp_over_Rs = Rp_over_a*a
+    Ms = 1.2*u.solMass # \pm 0.2
+    q = (Mp.to('kg')/Ms.to('kg')).value
+
+    vsini = 6500.*u.m/u.s # \pm 2000
+    Rs = 1.32*u.solRad #\pm 0.08
+    Omegas = vsini.to('m/s')/Rs.to('m')*per.to('s') # stellar rotation state - very little effect, so chosen arbitrarily
+
+    Ts = 6300. # \pm 200
+    Faigler_T0 = (737.49 + 2455000. - 2454833.)# % per.to('day').value # \pm0.19
+    T0 = 0.68508434
+
+    coeffs = [0.313, 0.304]
+
+    Aplanet = 60.4e-6 # \pm 2.0
+    F0 = 60.4e-6 # \pm 2.0 -- overall shift in light curve, which is arbitrary
+    phase_shift = -10.3/360. # \pm 2.0 - convert phase shift angle from degrees to orbital phase
+
+    beta = 0.0716671 # Interpolation from among the values reported in Claret & Bloemen (2011) A&A 529, 75
+
+    # Save parameters to an evilmc parameters object
+    saved_ep = evparams(per=per.to('day').value, a=a, T0=T0, p=Rp_over_Rs, 
+            limb_dark="quadratic", b=b, 
+            F0=F0, Aplanet=Aplanet, phase_shift=phase_shift, beta=beta, 
+            q=q, Kz=Kz, Ts=Ts, Ws=[0.,0.,Omegas], u=coeffs)
+
+    saved_params = {
+            "per": saved_ep.per,
+            "i": inc,
+            "a": saved_ep.a,
+            "T0": saved_ep.T0,
+            "p": saved_ep.p,
+            "linLimb": saved_ep.u[0],
+            "quadLimb": saved_ep.u[1],
+            "b": saved_ep.b,
+            "Aellip": 21.1e-6,
+            "Abeam": 13.5e-6,
+            "F0": 0.,
+            "Aplanet": 50.4e-6,
+            "phase_shift": saved_ep.phase_shift
+            }
+
+    # Estimate scatter
+    dur = transit_duration(saved_ep)
+
+def transit_indices(time, dur, T0):
+    # Return transit indices
+    return (np.abs(time - T0) < 0.5*dur)
 
 def calc_SR(time, flux, err):
     # Calculate the signal residue as defined in Kovacs+ (2002) -
